472053a4568bc5144377cf1410b397c051e8b4e1
jtimonen <juho.timonen@iki.fi>
2025-04-23 18:20:25 +0300

    create aaa.R and zzz.R

---
+++ R/aaa.R
@@ -0,0 +1,23 @@
+# MAIN DOCUMENTATION PAGE -------------------------------------------------
+
+#' The 'pkmstate' package.
+#'
+#' @description Bayesian multistate modeling using 'Stan'.
+#' @author Juho Timonen (first.last at iki.fi) and Jacki Buros
+#' @keywords multistate Stan Bayesian
+#'
+#' @section Getting started:
+#' See the following \code{R6} classes.
+#' \itemize{
+#'  \item \code{\link{PathData}}: Class to hold generated paths.
+#' }
+#'
+#'
+#' @name pkmstate-package
+#' @aliases pkmstate
+#' @import ggplot2 stats
+#' @importFrom R6 R6Class
+#' @importFrom posterior as_draws_rvars
+#'
+#'
+"_PACKAGE"
