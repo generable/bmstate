07247ef41ec305d9b2ae4c669f9d2f6505ef328b
jtimonen <juho.timonen@iki.fi>
2025-05-02 14:55:57 +0300

    cleanup

--- R/aaa.R
+++ R/aaa.R
@@ -58,14 +58,10 @@
 
 #' Creates the Stan model
 #'
-#' @param mixp use mixture prior?
 #' @export
 #' @return A CmdstanR model
 create_stan_model <- function(mixp = FALSE) {
-  fn <- "msm-general.stan"
-  if (mixp) {
-    fn <- "msm-general-mixp.stan"
-  }
+  fn <- "msm.stan"
   filepath <- system.file(file.path("stan", fn), package = "bmstate")
   cmdstanr::cmdstan_model(filepath)
 }
