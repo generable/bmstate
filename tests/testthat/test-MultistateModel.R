e2ec1590f1d380ffc9e070753e32af45e0a2c440
jtimonen <juho.timonen@iki.fi>
2025-05-05 04:19:22 +0300

    edit test

--- tests/testthat/test-MultistateModel.R
+++ tests/testthat/test-MultistateModel.R
@@ -7,18 +7,14 @@ test_that("MultistateModel init works", {
   covs <- list(hazard = "age")
   mod <- create_msm(a, nam, covs, compile = F)
   expect_true(inherits(mod, "MultistateModel"))
-  expect_output(mod$print())
+  expect_message(print(mod))
 })
 
 
-test_that("MultistateModel fitting works", {
-  dat <- simulate_example_data()
-
-  a <- matrix(0, 3, 3)
-  a[1, 2] <- 1
-  a[2, 3] <- 1
-  a[2, 2] <- 1
-  nam <- c("A", "B", "C")
-  covs <- list(hazard = "age")
-  mod <- create_msm(a, nam, covs)
+test_that("creating MultistateModel from PathData works", {
+  sim <- simulate_example_data(100, sys_idx = 2)
+  pd <- sim$pd
+  mod <- create_msm(a, nam, covs, compile = F)
+  expect_true(inherits(mod, "MultistateModel"))
+  expect_message(print(mod))
 })
