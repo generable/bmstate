a28ed2ba58a1bb28748256f90b41832baf33e413
jtimonen <juho.timonen@iki.fi>
2025-08-12 09:27:37 +0300

    use purrr

--- R/stan.R
+++ R/stan.R
@@ -91,13 +91,13 @@ fit_stan <- function(model, data, prior_only = FALSE,
   if (isFALSE(pathfinder)) {
     diag <- stan_fit$diagnostic_summary()
   }
-  smr <- capture.output({
-    smr <- stan_fit$summary()
-  })
+  sfun <- function(x) x$summary()
+  quiet_sumr <- purrr::quietly(sfun)
+  smr <- quiet_sumr(stan_fit)
   info <- list(
     diag = diag,
     runset = stan_fit$runset,
-    summary = smr
+    summary = smr$result
   )
   fit <- MultistateModelFit$new(data, sd, model, draws, info)
   if (!return_stanfit) {
